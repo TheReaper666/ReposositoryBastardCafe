d647b7a15e4e2a1792f6a2be53765fa4e3ebc374
TheReaper666 <jack0810@edu.campusvejle.dk>
2017-11-01 10:35:31 +0100

    ucOverViewRes Done

---
+++ Eksamen2016/UcOverViewRes.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eksamen2016
+{
+    /// <summary>
+    /// Interaction logic for ucOverWievRes.xaml
+    /// </summary>
+    public partial class ucOverWievRes : UserControl
+    {
+        public ucOverWievRes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpretRes(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DataGridOVR_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextboxSøgefelt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
