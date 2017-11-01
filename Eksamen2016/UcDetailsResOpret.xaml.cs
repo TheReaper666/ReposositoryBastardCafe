e371fc5e93320cd68a79d59cf00ed852209a4ccf
Baron Daniel Bernhard Rugbjerg Gammelgaard Giversen <bern0145@edu.campusvejle.dk>
2017-11-01 10:57:44 +0100

    UcOverviewDetails oprettet

---
+++ Eksamen2016/UcDetailsResOpret.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eksamen2016
+{
+    /// <summary>
+    /// Interaction logic for UcDetailsResOpret.xaml
+    /// </summary>
+    public partial class UcDetailsResOpret : UserControl
+    {
+        public UcDetailsResOpret()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGridCostumerList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonCreateCostumer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonCreateReservation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
