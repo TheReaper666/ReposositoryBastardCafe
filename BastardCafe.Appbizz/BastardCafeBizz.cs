1f9af031e026f602b62200594c8fe2899634ecd6
TheReaper666 <jack0810@edu.campusvejle.dk>
2017-11-02 15:00:51 +0100

    Abejder på AppBizz

--- BastardCafe.Appbizz/BastardCafeBizz.cs
+++ BastardCafe.Appbizz/BastardCafeBizz.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,19 @@ namespace BastardCafe.Appbizz
         public BastardCafeBizz()
         {
 
+        }
+
+        public void FillReservationData()
+        {
+            DataSet DSReservation;
+        }
+        public void FillKundeData()
+        {
+            DataSet DSKundeData;
+        }
+        public void FillSpilData()
+        {
+            DataSet DSSpilData;
         }
     }
 }
