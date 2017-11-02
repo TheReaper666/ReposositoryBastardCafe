7ded34e71e0ca2230606f954a0f323f04a7b01b2
Baron Daniel Bernhard Rugbjerg Gammelgaard Giversen <bern0145@edu.campusvejle.dk>
2017-11-02 11:40:48 +0100

    SpilBeskrivelse - "færdig"

--- BastardCafe.Appbizz/SpilBeskrivelse.cs
+++ BastardCafe.Appbizz/SpilBeskrivelse.cs
@@ -8,15 +8,27 @@ namespace BastardCafe.Appbizz
 {
     public class SpilBeskrivelse
     {
-        private int beskrivelseId;
+        #region Fields
         private string beskrivelse;
+        private int beskrivelseId;
+        #endregion
 
+        #region Constructors
         public SpilBeskrivelse()
         {
 
         }
 
-        public int BeskrivelseId { get => beskrivelseId; set => beskrivelseId = value; }
-        public string Beskrivelse { get => beskrivelse; set => beskrivelse = value; }
+        public SpilBeskrivelse(string beskriv, int beskId)
+        {
+            beskrivelse = beskriv;
+            beskrivelseId = beskId;
+        }
+        #endregion
+
+        #region Properties
+        public int BeskrivelseId { get => beskrivelseId; }
+        public string Beskrivelse { get => beskrivelse; }
+        #endregion
     }
 }
