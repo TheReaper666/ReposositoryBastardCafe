5673b9702b6485939658a29a45a2de375960b29e
EmilC-Aspit <31403281+EmilC-Aspit@users.noreply.github.com>
2017-11-02 14:13:59 +0100

    Res og spil foreløbig

--- BastardCafe.Appbizz/Resevartion.cs
+++ BastardCafe.Appbizz/Resevartion.cs
@@ -8,9 +8,67 @@ namespace BastardCafe.Appbizz
 {
     class Resevartion
     {
+        private int resevartionsID;
+        public Bord bord;
+        public Kunde kunde;
+        public Spil spil;
+        private DateTime resavationsDato;
         public Resevartion()
         {
+            
+        }
 
+        public Resevartion(int bordID, int kundeID, int spilID,DateTime resavationsDato)
+        {
+            SpilSetup(spilID);
+            KundeSetup(kundeID);
+            BordSetup(bordID);
+            ResavationsDato = resavationsDato;
+        }
+
+        public Resevartion(int resevationsID)
+        {
+            ResevartionsID = resevationsID;
+        }
+         
+        private void SpilSetup(int spilID)
+        {
+           
+        }
+
+        private void KundeSetup(int kundeID)
+        {
+
+        }
+
+        private void BordSetup(int bordID)
+        {
+
+        }
+
+
+        public int ResevartionsID
+        {
+            get
+            {
+                return resevartionsID;
+            }
+            private set
+            {
+                resevartionsID = value;
+            }
+        }
+
+        public DateTime ResavationsDato
+        {
+            get
+            {
+                return resavationsDato;
+            }
+            private set
+            {
+                resavationsDato = value;
+            }
         }
     }
 }
--- BastardCafe.Appbizz/Spil.cs
+++ BastardCafe.Appbizz/Spil.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,40 @@ namespace BastardCafe.Appbizz
 {
     class Spil
     {
+        public SpilNavn spilNavn;
+        public SpilGenre spilGerne;
+        public SpilBeskrivelse spilBeskrivelse;
+        public SpilDeltagerAntal spilDeltagerAntal;
+        public SpilNormTid spilNormTid;
+        private int spilID;
+
         public Spil()
         {
 
         }
+
+        public Spil(DataSet data)
+        {
+            SetupSpilClass(data);
+        }
+
+        private void SetupSpilClass(DataSet ds)
+        {
+            
+        }
+
+
+        public int SpilID
+        {
+            get
+            {
+                return spilID;
+            }
+            private set
+            {
+                spilID = value;
+            }
+        }
+
     }
 }
