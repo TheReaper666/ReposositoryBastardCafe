e34809fd5af5001735bec7acb7d71eba6279105d
TheReaper666 <jack0810@edu.campusvejle.dk>
2017-11-21 15:44:01 +0100

    make

--- BastardCafe.IO/BastardDB.cs
+++ BastardCafe.IO/BastardDB.cs
@@ -14,38 +14,23 @@ namespace GeneralDB.IO
     {
         public BastardDB()
         {
-            ConnectionString = "Server=10.205.44.39,49172;Database=BastardCafeM2E202017;User Id=Aspit;Password=Server2012;";
+            ConnectionString = "Server=10.205.44.39,49172;Database=LoginSystemDB;User Id=Aspit;Password=Server2012;";
         }
 
-        public DataTable DTGetKundeData()
+        public DataTable DTGetUserData()
         {
             string SQLquery = $"SELECT Kunder.id, PostBy.ByNavn " +
             $"FROM Kunder INNER JOIN PostBy ON Kunder.PostNr = PostBy.PostNr";
             return DbReturnDataTable(SQLquery);
         }
-        public DataTable DTGetSpilData(bool ValgSpilStatus)
+        public DataTable DTGetLoginData()
         {
-            string SQLquery = $"SELECT Spil.Spilid, SpilNavn.SpilNavn, SpilGenre.Genre, SpilDeltagerAntal.SpilDeltagerAntal," +
-               $" SpilTid.SpilTid, SpilBeskrivelse.SpilBeskrivelse FROM Spil INNER JOIN SpilBeskrivelse" +
-               $" ON Spil.Spilid = SpilBeskrivelse.Spilid INNER JOIN SpilDeltagerAntal" +
-               $" ON Spil.DeltagerId = SpilDeltagerAntal.Deltagerid INNER JOIN SpilGenre" +
-               $" ON Spil.Genreid = SpilGenre.Genreid INNER JOIN SpilNavn" +
-               $" ON Spil.NavnId = SpilNavn.Navnid INNER JOIN SpilTid" +
-               $" ON Spil.NormalTidId = SpilTid.Tidid WHERE (Spil.SpilStatus = '{ValgSpilStatus}')";
+            string SQLquery = $"SELECT Spil.Spilid, SpilNavn.SpilNavn, SpilGenre.Genre, SpilDeltagerAntal.SpilDeltagerAntal,";
             return DbReturnDataTable(SQLquery);
         }
         public DataTable DTGetResevartionData(bool ValgtSpilStatus)
         {
-            string SQLquery = $"SELECT Bord.Pladser, BordRes.id, Spil.id AS SpilID, SpilNavn.SpilNavn, SpilTid.SpilTid, " +
-                $"SpilDeltagerAntal.SpilDeltagerAntal, SpilGenre.Genre, SpilBeskrivelse.SpilBeskrivelse, BordRes.resDate, " +
-                $"Kunder.FuldeNavn, Kunder.Adresse, PostBy.ByNavn, Kunder.Mobil, Kunder.Email, Kunder.GamerNavn, " +
-                $"Spil.SpilStatus FROM Bord RIGHT OUTER JOIN BordRes ON Bord.id = BordRes.resBord LEFT OUTER JOIN " +
-                $"PostBy RIGHT OUTER JOIN Kunder ON PostBy.PostNr = Kunder.PostNr ON BordRes.resKunde = Kunder.id " +
-                $"LEFT OUTER JOIN SpilBeskrivelse RIGHT OUTER JOIN Spil ON SpilBeskrivelse.Spilid = Spil.Spilid " +
-                $"LEFT OUTER JOIN SpilGenre ON Spil.Genreid = SpilGenre.Genreid LEFT OUTER JOIN SpilDeltagerAntal ON " +
-                $"Spil.DeltagerId = SpilDeltagerAntal.Deltagerid LEFT OUTER JOIN SpilTid ON Spil.NormalTidId = " +
-                $"SpilTid.Tidid LEFT OUTER JOIN SpilNavn ON dbo.Spil.NavnId = SpilNavn.Navnid ON dbo.BordRes.resSpil = dbo.Spil.id " +
-                $"WHERE (Spil.SpilStatus = '{ValgtSpilStatus}')";
+            string SQLquery = $"SELECT Bord.Pladser, BordRes.id, Spil.id AS SpilID, SpilNavn.SpilNavn, SpilTid.SpilTid,";
             return DbReturnDataTable(SQLquery);
         }
 
